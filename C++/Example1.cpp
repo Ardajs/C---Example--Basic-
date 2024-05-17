ad5513bbdd1a17e28e30c2199720d3c3bdda9683
Arda <ardaalaann@gmail.com>
2024-05-17 18:50:25 +0300

    C++ Example
    C++ ile 3 öğrencinin notlarının ortamasını hesaplayan koddur.

---
+++ C++/Example1.cpp
@@ -0,0 +1,21 @@
+﻿// Girilen 3 yazılı notunun ortalamasını hesaplayan ve gösteren c++ kodu
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int s1, s2, s3;
+	double ort;
+	cout << "Lutfen 1. notu giriniz:";
+	cin >> s1;
+	cout << "Lutfen 2. notu giriniz:";
+	cin >> s2;
+	cout << "Lutfen 3. notu giriniz:";
+	cin >> s3;
+	cout << endl;
+	ort = (double)(s1 + s2 + s3) / 3;
+	cout << "Not Ortalamasi:" << ort;
+	return 0;
+
+}
