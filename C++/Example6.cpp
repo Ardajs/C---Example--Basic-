b9e3d8070a3e6f9b2735c8c0ae6d130ca73066ae
Arda <ardaalaann@gmail.com>
2024-05-18 13:48:07 +0300

    C_plusplus

---
+++ C++/Example6.cpp
@@ -0,0 +1,34 @@
+﻿/*klavyeden girilen uzun ve kısa kenarlı dikdörtgenin alanı ve çevresini hesapla*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int uzunkenar, kisakenar;
+	int alan, cevre;
+	cout << "Lutfen dikdortgenin kenarlarini giriniz..." << endl;
+	cout << "Lutfen uzun kenari giriniz:";
+	cin >> uzunkenar;
+	cout << endl;
+	cout << "Lutfen dikdörtgenin kisa kenarini giriniz:";
+	cin >> kisakenar;
+	cout << endl;
+
+	char secim;
+	cout << "Dikdortgenin Alanini Hesaplamak istiyorsaniz 0 i secin" << endl;
+	cout << "Dikdortgenin Cevresini Hesaplamak istiyorsanz 1 i secin" << endl;
+	cin >> secim;
+	if (secim == '0') {
+		alan = uzunkenar * kisakenar;
+		cout << alan << endl;
+	}
+	if (secim == '1') {
+		cevre = 2*(uzunkenar + kisakenar);
+		cout << cevre << endl;
+	}
+	return 0;
+
+
+}
---
+++ C++/Girilen iki tam sayı arasında kalan tam sayıların toplamı.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i, baslangic, son;
+	int toplam = 0;
+	cout << "Baslangic degeri:";
+	cin >> baslangic;
+	cout << "son degeri:";
+	cin >> son;
+
+	for (i=baslangic + 1; i < son; i++) 
+		toplam += i;
+		cout << "sonuc:" << toplam << endl;
+	
+}
